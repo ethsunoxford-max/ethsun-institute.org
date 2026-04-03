eda4c1dc14859529f1e4fb8639610a557b6c20b9
ethsunoxford-max <ethsunoxford@gmail.com>
2026-04-03 07:51:08 +0100

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,19 @@
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  images: {
+    formats: ['image/webp'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default withNextIntl(nextConfig);
