26e7d4764ca1a9ce85be113b19de096e6b537c0a
ethsunoxford-max <ethsunoxford@gmail.com>
2026-04-06 23:53:12 +0100

    feat: add E shield logo next to horizontal logo in navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -50,13 +50,21 @@ export default function Navbar() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16 lg:h-20">
             {/* Logo */}
-            <Link href="/" className="flex-shrink-0">
+            <Link href="/" className="flex-shrink-0 flex items-center gap-2">
+              <Image
+                src="/images/logo-e-shield.jpeg"
+                alt="ETHSUN"
+                width={36}
+                height={36}
+                className="h-8 lg:h-9 w-auto rounded-sm"
+                priority
+              />
               <Image
                 src="/images/logo-horizontal.png"
                 alt="ETHSUN Institute"
-                width={180}
-                height={40}
-                className="h-8 lg:h-10 w-auto"
+                width={160}
+                height={36}
+                className="h-7 lg:h-8 w-auto hidden sm:block"
                 priority
               />
             </Link>
